844fa48e19503f622a63762ee31052ee6069535c
yalamix <yalamix@icloud.com>
2025-02-26 21:03:48 -0300

    3rd question improv + readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# LeetCode 150
+
+A repository of my solutions to the [Top Interview 150](https://leetcode.com/studyplan/top-interview-150/) list of problems from LeetCode.
+
+## Notes
+- All problems were done by myself, without consulting any outside sources. Everything straight out of my brain.
+- I do not have a time limit to solve them.
+- I'm doing this for fun and to keep my mind sharp as well as my coding skills.
+- I always try to code up a solution that places me in the top result, but my initial solutions are also included, commented.
--- array_string/26_remove_duplicates_from_sorted_array.py
+++ array_string/26_remove_duplicates_from_sorted_array.py
@@ -1,19 +1,35 @@
 from typing import List
 
+## FIRST ACCEPTED SOLUTION
+
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         last = None
+#         n = len(nums)
+#         k = 0
+#         for i in range(n):
+#             if nums[k] == last:
+#                 nums.pop(k)
+#                 nums.append(0)
+#             else:
+#                 last = nums[k]
+#                 k += 1
+#         return k
+
+## SECOND ACCEPTED SOLUTION (FINAL)
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        last = None
+        last = nums[0]
         n = len(nums)
-        k = 0
-        for i in range(n):
-            if nums[k] == last:
-                nums.pop(k)
-                nums.append(0)
-            else:
-                last = nums[k]
+        k = 1
+        for i in range(1, n):
+            if nums[i] != last:
+                last = nums[i]
+                nums[k] = last
                 k += 1
-        return k
-      
+        return k     
+    
 nums = [0,0,1,1,1,2,2,3,3,4]
 
 s = Solution()
